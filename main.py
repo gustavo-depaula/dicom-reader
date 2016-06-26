b41b6baec7e0fd556f148c4d6fe280b0fb4b14f4
Gustavo de Paula <gdpaula2000@gmail.com>
2016-06-25 21:49:29 -0300

    2 commit

--- main.py
+++ main.py
@@ -1,55 +1,135 @@
 # coding=utf-8
 # -*- coding: utf-8 -*-
 
-from PyQt4 import QtGui # Importa o módulo do PyQT que precisamos
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtGui import QApplication
+import glob
 import dicom
-import sys # Pegar infos do sistema
+import sys  # Pegar infos do sistema
+from natsort import natsorted
 import os
+import vtk
+import numpy
+from design import design  # Neste arquivo está a MainWindow que foi desenhada
 
-from design import design # Neste arquivo está a MainWindow que foi desenhada
+dcmFiles = [] # lista onde guarda os arquivos da pasta selecionada
+index = 0
+ArrayDicom = []
 
-
-class ChooseFile(QtGui.QMainWindow, design.Ui_MainWindow):
-    def __init__(self):
-        # permite que acessamos variáveis e métodos do arquivo design.py
+class MainWindow(QtGui.QMainWindow, design.Ui_MainWindow):
+    def __init__(self, parent=None):
         super(self.__class__, self).__init__()
-        self.setupUi(self)  # isso é definido no design.py
+        self.setupUi(self)
+        self.setWindowTitle("Project N")
+        self.setWindowIcon(QtGui.QIcon('design\\brain_icon2.png'))
 
-        self.btnBrowse.clicked.connect(self.browse_folder) # quando o botão for apertado executa a função
-        self.btnSelect.clicked.connect(self.select_file)
+        self.actionSelecionar_Pasta.triggered.connect(self.browse_folder)
+        self.btnLeft.clicked.connect(self.toTheLetf)
+        self.btnRight.clicked.connect(self.toRight)
+
+        self.hSlider.valueChanged.connect(self.changeWithSlider)
+
+
+
+        global ArrayDicom
+        PathDicom = "./dicom_images/"
+        lstFilesDCM = []  # create an empty list
+        for dirName, subdirList, fileList in os.walk(PathDicom):
+            for filename in fileList:
+                if ".dcm" in filename.lower():  # check whether the file's DICOM
+                    lstFilesDCM.append(os.path.join(dirName, filename))
+
+        # Get ref file
+        RefDs = dicom.read_file(lstFilesDCM[0])
+
+        # Load dimensions based on the number of rows, columns, and slices (along the Z axis)
+        ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(lstFilesDCM))
+
+        # Load spacing values (in mm)
+        ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), float(RefDs.SliceThickness))
+
+        x = numpy.arange(0.0, (ConstPixelDims[0] + 1) * ConstPixelSpacing[0], ConstPixelSpacing[0])
+        y = numpy.arange(0.0, (ConstPixelDims[1] + 1) * ConstPixelSpacing[1], ConstPixelSpacing[1])
+        z = numpy.arange(0.0, (ConstPixelDims[2] + 1) * ConstPixelSpacing[2], ConstPixelSpacing[2])
+
+        # The array is sized based on 'ConstPixelDims'
+        ArrayDicom = numpy.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
+
+        # loop through all the DICOM files
+        for filenameDCM in lstFilesDCM:
+            # read the file
+            ds = dicom.read_file(filenameDCM)
+            # store the raw image data
+            ArrayDicom[:, :, lstFilesDCM.index(filenameDCM)] = ds.pixel_array
+
+
+    def dumpDicomImg(self, index):
+        global ArrayDicom
+        imv = self.graphicsView
+        imv.setImage(ArrayDicom[:, :, index])
+        imv.getView()
 
     def browse_folder(self):
-        self.listWidget.clear() # limpa a lista
-        directory = QtGui.QFileDialog.getExistingDirectory(self, "Escolha uma pasta!")
+        global dcmFiles
+        dcmFiles = natsorted(glob.glob("dicom_images\*.dcm"))
+        self.hSlider.setMaximum(len(dcmFiles) - 1)
+        self.dumpDicomInfo(0)
+        self.dumpDicomImg(0)
+        #directory = QtGui.QFileDialog.getExistingDirectory(self, "Escolha uma pasta!")
 
-        if directory: # se usuário n escolheu, n continua
-            for file_name in os.listdir(directory):
-                self.listWidget.addItem(file_name)
+        #path = unicode(directory.toUtf8(), encoding="UTF-8") + "/*.dcm"
+        #dcmFiles = glob.glob(path)
 
-    def select_file(self):
-        self.listDicom.clear()
-        file = QtGui.QFileDialog.getOpenFileName(self, 'Escolha um arquivo DICOM.')
+    def toRight(self):
+        global index
+        if index + 1 != len(dcmFiles):
+            index += 1
+            self.hSlider.setValue(index)
+            self.dumpDicomInfo(index)
+            self.dumpDicomImg(index)
 
-        print "%s" % file
+    def toTheLetf(self):
+        global index
+        if index > 0:
+            index = index - 1
+            self.hSlider.setValue(index)
+            self.dumpDicomInfo(index)
+            self.dumpDicomImg(index)
+
+    def changeWithSlider(self):
+        global index
+        index = self.hSlider.value()
+        self.dumpDicomInfo(index)
+        self.dumpDicomImg(index)
+
+    def dumpDicomInfo(self, index):
+        self.listInfo.clear()
+
+        file = dcmFiles[index]
+        self.listInfo.addItem("file: %s" % file)
         data = dicom.read_file("%s" % file)
+
+        keysWeWant = ['PatientName', 'PatientSex', 'PatientBirthDate', 'PerformedProcedureStepDescription', 'PerformedStationAETitle', 'InstitutionName', 'SliceLocation', 'SliceThickness', 'SpacingBetweenSlices']
         for key in data.dir():
             value = getattr(data, key, '')
             if type(value) is dicom.UID.UID or key == "PixelData":
                 continue
 
-            self.listDicom.addItem("%s : %s" % (key, value))
+            for k in keysWeWant:
+                if key == k:
+                    self.listInfo.addItem("%s : %s" % (key, value))
+
 
 def main():
-    #
     reload(sys)
     sys.setdefaultencoding('utf8')
-    # para o programa reconhecer utf-8
 
     app = QtGui.QApplication(sys.argv)  # uma nova instância do QApplication
-    form = ChooseFile()                 # seta este formulários para o que esta no design
-    form.show()                         # mostra o form
-    app.exec_()                         # e executa o app
+    form = MainWindow()  # seta este formulários para o que esta no design
+    form.show()  # mostra o form
+    # form.iren.Initialize()
+    app.exec_()  # e executa o app
 
 
-if __name__ == '__main__':              # if we're running file directly and not importing it
+if __name__ == '__main__':
     main()
---
+++ main.spec
@@ -0,0 +1,33 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+
+a = Analysis(['..\\prot\xf3tipo neuro 1\\main.py'],
+             pathex=['C:\\Users\\gdpau\\Desktop\\PROTTI~1'],
+             binaries=None,
+             datas=None,
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name='main',
+          debug=False,
+          strip=False,
+          upx=False,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=False,
+               name='main')
