7a60cf070ef08bcc4d401f7056a75909f110531d
Gustavo de Paula <gdpaula2000@gmail.com>
2016-10-25 14:59:40 -0200

    COMMITADO

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# made by gustavo-depaula
 import sys
 from cx_Freeze import setup, Executable
 
@@ -6,3 +7,8 @@ setup(
     version = "0.1",
     description = "Primeiro Prototipo",
     executables = [Executable("main.py", base = "Win32GUI")])
+Gustavo de Paula:
+#     Comecei o protótipo com uma interface gráfica bem simples
+#     onde o usuário pode escolher uma pasta para ver o contido
+#     e pode selecionar um arquivo DICOM para ler todas as info_
+#     rmações possí
